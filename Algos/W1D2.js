1c4e0361af007ce1a140d17ea2faf73828f1ceb0
Sloan Molstad <sloan.molstad@gmail.com>
2023-01-24 19:47:08 -0800

    inital commit

---
+++ Algos/W1D2.js
@@ -0,0 +1,16 @@
+// var creates a variable, temporarily holds a value
+var a = 25;
+
+a = a - 13;
+
+// one = means assignment operator
+// Js is a loosely typed language
+// variable's data type is applied when instantiating the variable
+
+// javascript data types:
+// integer/number: whole number
+// float: decimals/fractions
+/* strings: collection of characters such as words/text; can hold numbers
+*\ but they arent used as numbers
+// Boolean: T or F; (value 1 and 0); holding data type
+// concatonation: to put strings together
